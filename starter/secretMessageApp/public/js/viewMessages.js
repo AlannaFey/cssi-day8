c17b58c05210d04872605a710b9857eb1f0af525
AlannaFey <alanna.fey.hill@gmail.com>
2021-07-21 17:56:02 +0000

    secret message code

---
+++ starter/secretMessageApp/public/js/viewMessages.js
@@ -0,0 +1,19 @@
+const findMessages = (myPass) => {
+    const messagesRef = firebase.database().ref();
+    messagesRef.on('value',(snapshot) => {
+        const data = snapshot.val();
+        console.log(data);
+        for(let key in data) {
+            const message = data[key];
+            console.log(message);
+            if(myPass == message.passcode) {
+            renderMessage(message);
+            }
+        }
+    });
+}
+
+document.querySelector("viewMsg").addEventListener("click" , () => {
+  const passcode = document.querySelector("#passcode").value;
+  findMessages(passcode);
+});
